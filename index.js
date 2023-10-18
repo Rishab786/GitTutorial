9756c81b4ef643537e208258c3b96be6c9c14f29
rishab786 <rishabsingla5@gmail.com>
2023-10-18 18:47:42 +0530

    comit3

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-document.getElementById("items").style.fontWeight = "900";
-document.getElementById("third").style.backgroundColor='green';
+var elements=document.getElementsByClassName("list-group-item");
+for(let i=0;i<elements.length;i++)
+{
+    elements[i].style.backgroundColor='green';
+}
+var items=document.getElementsByTagName('li');
+items[4].style.fontWeight = "900";;
 
