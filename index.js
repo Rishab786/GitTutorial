f9582086608623c9eb192c41e6825f53949b8ae9
rishab786 <rishabsingla5@gmail.com>
2023-10-18 20:34:32 +0530

    comit5

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-var elements=document.getElementsByClassName("list-group-item");
-elements[1].style.backgroundColor='green';
-elements[2].hidden = true;
-var items=document.querySelectorAll(".list-group-item");
-items[1].style.color='green';
-for(let i=0;i<items.length;i++)
-{
-    if(i%2==0)
-    {
-        items[i].style.backgroundColor='green';
-    }
-}
+
+document.querySelector('#items').parentElement.style.backgroundColor='yellow';
+document.querySelector(".list-group-item").nextElementSibling.style.backgroundColor='red';
+document.querySelector('#items').previousElementSibling.style.backgroundColor='green';
+document.querySelector('#items').firstElementChild.style.color='blue';
+document.querySelector('#items').lastElementChild.textContent="hello";
+
+let newItem=document.createElement('div');
+let newItemText=document.createTextNode('hello world');
+newItem.appendChild(newItemText);
+let container=document.querySelector('header .container');
+let h1=document.querySelector('header h1');
+container.insertBefore(newItem,h1);
+let secondItem=document.createElement('div');
+secondItem.innerText='hello world';
+let list=document.querySelector('#items');
+list.insertAdjacentElement('afterbegin',secondItem);
+
+
 
 
 
