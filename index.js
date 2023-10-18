6870d145055d2869f8bf65b03b179a5763d639ee
rishab786 <rishabsingla5@gmail.com>
2023-10-18 19:03:41 +0530

    comit4

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var elements=document.getElementsByClassName("list-group-item");
-for(let i=0;i<elements.length;i++)
+elements[1].style.backgroundColor='green';
+elements[2].hidden = true;
+var items=document.querySelectorAll(".list-group-item");
+items[1].style.color='green';
+for(let i=0;i<items.length;i++)
 {
-    elements[i].style.backgroundColor='green';
+    if(i%2==0)
+    {
+        items[i].style.backgroundColor='green';
+    }
 }
-var items=document.getElementsByTagName('li');
-items[4].style.fontWeight = "900";;
+
+
 
