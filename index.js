6f062fc66807748674504cbaab2a9357cf48efed
rishab786 <rishabsingla5@gmail.com>
2023-10-19 12:09:52 +0530

    finalCommit

--- index.js
+++ index.js
@@ -7,16 +7,25 @@ let name=document.getElementById('name').value;
 let mail=document.getElementById('email').value;
 let time=document.getElementById('time').value;
 let date=document.getElementById('date').value;
-    let obj={
+    var obj={
         Name:name,
         Email:mail,
         Date:date,
         Time:time
-        
-
     };
-    localStorage.setItem('user',obj);
-    console.log(obj);
+    
+    localStorage.setItem(mail,JSON.stringify(obj));
+    const keys = Object.keys(localStorage);
+    for(let i=0;i<keys.length;i++)
+    {
+       let object= localStorage.getItem(keys[i]);
+       let component=document.getElementById('uList');
+       let newElement=document.createElement('li');
+       newElement.textContent=object;
+       component.appendChild(newElement);
+       
+    }
+    
 
 
 }
