3d03db538363d20eb13b39fcad3e458630a78805
rishab786 <rishabsingla5@gmail.com>
2023-10-19 10:39:29 +0530

    commit8

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 let form=document.getElementById('submission');
 form.addEventListener('submit', addItem);
+
 function addItem(e){
     e.preventDefault();
-    localStorage.setItem('name',document.getElementById('name').value);
-    localStorage.setItem('email',document.getElementById('email').value);
-    localStorage.setItem('date',document.getElementById('date').value);
-    localStorage.setItem('time',document.getElementById('time').value);
+let name=document.getElementById('name').value;
+let mail=document.getElementById('email').value;
+let time=document.getElementById('time').value;
+let date=document.getElementById('date').value;
+    let obj={
+        Name:name,
+        Email:mail,
+        Date:date,
+        Time:time
+        
+
+    };
+    localStorage.setItem('user',obj);
+    console.log(obj);
 
 
 }
